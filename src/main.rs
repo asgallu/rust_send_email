4574e535d352671af0b7d6f2886ff49059bb6162
JavierGonzalezAlvarez <jgalvarez.jga@gmail.com>
2020-11-25 12:24:34 +0100

    email_v2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+// rust_email/main.rs
+
+//https://blog.logrocket.com/email-crates-for-rust-lettre-and-imap/
+
+//https://crates.io/crates/lettre
 extern crate lettre;
 extern crate lettre_email;
 
 //use lettre::{EmailTransport, SmtpTransport};
 use lettre::{SmtpClient, Transport};
 use lettre_email::EmailBuilder;
+
+//https://docs.rs/lettre/0.9.2/lettre/all.html
 use lettre::smtp::authentication::Credentials;
 //para fichero adjuntos
 //use std::path::Path;
@@ -11,11 +18,23 @@ use lettre::smtp::authentication::Credentials;
 fn main() {
     let email = EmailBuilder::new()
         // Addresses can be specified by the tuple (email, alias)
-        .to(("info@spotawire.com", "Info - Spot a Wire"))
+        //.to(("user@example.org", "Firstname Lastname"))
+        .to("info@spotawire.com")
+        //copia oculta
+        .bcc("jgalvarez.jga@gmail.com")
         // ... or by an address only
         .from("info@spotawire.com")
-        .subject("Hi, Hello world from rust")
-        .text("Hello world. Rust_1")
+        .subject("Confirmación de formulario/email enviado - Spot a Wire")
+        //.text("Hello world. Rust_1")
+        .html(" 
+            <div>               
+                Buenos días,<br><br>
+                Gracias por ponerte en contacto con nosotros.<br>
+                <a>En breve te contestaremos.</a><br><br>
+                <a>Recibe un cordial saludos</a><br>
+                <a href='www.spotawire.com'>www.spotawire.com</a>
+            </div>               
+            ")
         .build()
         .unwrap();
     let mut mailer = SmtpClient::new_simple("ssl0.ovh.net")
@@ -26,9 +45,9 @@ fn main() {
     // Send the email
     let result = mailer.send(email.into());
     if result.is_ok() {
-        println!("Email sent");
+        println!("Email enviado");
     } else {
-        println!("Could not send email: {:?}", result);
+        println!("No se puede enviar email: {:?}", result);
     }
     assert!(result.is_ok());
 }
